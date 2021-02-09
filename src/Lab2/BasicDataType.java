60e786fc2c505780276c83edd906fc2f1a5276b3
chiranan <chiranan141414@gmail.com>
2021-02-09 10:17:27 +0700

    test

--- src/Lab2/BasicDataType.java
+++ src/Lab2/BasicDataType.java
@@ -1,40 +1,40 @@
 package Lab2;
 
 public class BasicDataType {
+
     public static void main(String[] args) {
 
-        //boolean => T,F (1,0)
+        // boolean => T,F (1,0)
         boolean myboo = true;
         System.out.println(myboo);
         myboo = false;
         System.out.println(myboo);
 
-        //character => only 1 character such as 'a' , 'x' , 's'
+        //character  => only 1 character such as 'a' , 'x', '$'
         char c = 'a';
         System.out.println(c);
 
-        //integer => -2 -1 0 1 2 3 ...10000
+        //integer  => -2 -1 0 1 2 3 ... 10000
         int number;
         number = 100;
-        System.out.println(number); //100
-        System.out.println(number*5); //500
+        System.out.println(number); // 100
+        System.out.println(number*5);  // 500
         //number = ? 100
 
         //real number => float, double => 10.11, -22.1
         float a = 10.10f;
         double b = 20.20;
-        System.out.println(a+b); //30.30
+        System.out.println(a+b);  //30.30
 
-        //string => message => "..." => "Hello"
-        //string คือ ข้อมูลชนิดข้อความ ที่มีตัวอักษรเรียงตัวกัน
+        //String => message => "...." => "Hello"
+        //String  คือ ข้อมูลชนิดแบบข้อความ ที่มีตัวอักษรต่อเรียงกัน
 
         String msg = "Hello RUTS Saiyai 2020";
         System.out.println(msg);
         System.out.println(msg.toUpperCase());
         System.out.println(msg.length());
 
-        //constant variaber
-
+        //constant variable
 
         final int num = 100;
 
@@ -43,8 +43,6 @@ public class BasicDataType {
 
 
 
-    }//mai
-    // n
-
+    }//main
 
 }//class
--- src/Lab2/Exercise_STD_Profile.java
+++ src/Lab2/Exercise_STD_Profile.java
@@ -1,36 +1,17 @@
 package Lab2;
-
 import java.util.Scanner;
 
 public class Exercise_STD_Profile {
+
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-
-        System.out.println("Enter an name: ");
-        String name = scanner.next();
-
-
-        System.out.println("Enter an lastname: ");
-        String lastname = scanner.next();
-
-
-        System.out.println("Enter an id: ");
-        String std_id = scanner.next();
-
-
-        System.out.println("Enter an age: ");
-        int age = scanner.nextInt();
-
-
-        System.out.println("Enter an email: ");
-        String email = scanner.next();
+        System.out.print("Enter integer: ");
+        int x = Integer.parseInt(sc.nextLine());
 
 
 
 
     }
-
-
 }
